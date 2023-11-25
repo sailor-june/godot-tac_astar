5129f52eaa8e0b23a8eb98cd3f48395ed94dd1a5
Juniper James <vvlpespro@gmail.com>
2023-11-25 11:01:48 -0500

    pathfinding with passable cells

--- Character.gd
+++ Character.gd
@@ -11,26 +11,38 @@ var movePoint = Vector2i.ZERO
 
 func _ready() -> void:
 	
+	#instantiate Astargrid
 	astargrid = AStarGrid2D.new()
 	astargrid.region =  grid.get_used_rect()
 	astargrid.cell_size = Vector2i(64,64)
-	
-	#manhattan distance, no diagonals
 	astargrid.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_NEVER
 	astargrid.update()
 	
 	
+	for x in grid.get_used_rect().size.x:
+		for y in grid.get_used_rect().size.y:
+			var tile_position = Vector2i(
+				x + grid.get_used_rect().position.x,
+				y +grid.get_used_rect().position.y
+			)
+			
+			var tile_data = grid.get_cell_tile_data(0, tile_position)
+			
+			if tile_data == null or tile_data.get_custom_data("walkable")==false:
+				astargrid.set_point_solid(tile_position)
+		
+	
+	#center yourself (-_-)
 	position = grid.map_to_local(position)
 	movePoint = position
 	
 	
 func _physics_process(delta):
-	
 	if current_id_path.is_empty():
+		#no active path.
 		return
 	
 	var target_position = grid.map_to_local(current_id_path.front())
-
 	position = position.move_toward(target_position,move_speed*get_process_delta_time())
 	if position ==  target_position:
 		current_id_path.pop_front()
@@ -50,14 +62,3 @@ func set_selected(value: bool) -> void:
 	isSelected = value
 	print("Selected:", name,  isSelected)
 
-#func follow_path(current_path, current_path_index):
-#
-#	if current_path_index > current_path.size()+1:
-#		current_path=null
-#		return
-#	current_path_index+=1
-#	movePoint = grid.map_to_local(current_path[current_path_index])
-#	print(movePoint)
-#	print(current_path_index)
-#	print(current_path)
-#	return
